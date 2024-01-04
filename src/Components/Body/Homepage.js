d4a83d8d800180bbf59853760f3dd4e8fcc29298
Blaze <32179683+leon-fenzl@users.noreply.github.com>
2024-01-03 21:36:15 -0300

    Update React + Game Test
    testing iframe callback to itch.io link onto a react page.

--- src/Components/Body/Homepage.js
+++ src/Components/Body/Homepage.js
@@ -3,7 +3,9 @@ import "../../Styles/body.css";
 function Homepage(){
   return(
     <section className="title">
-
+      <iframe frameborder="0" src="https://itch.io/embed-upload/9413682?color=333333" allowfullscreen="" width="1280" height="720">
+        <a href="https://blazingworks.itch.io/networkedplay">Play Networkedplay on itch.io</a>
+      </iframe>
     </section>
   );
 }
