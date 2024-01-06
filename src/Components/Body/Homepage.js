9dcc4188773cf59738975edd120b37af43c0a6cd
Blaze <32179683+leon-fenzl@users.noreply.github.com>
2024-01-05 21:12:21 -0300

    iframe Unity test 2

--- src/Components/Body/Homepage.js
+++ src/Components/Body/Homepage.js
@@ -3,9 +3,7 @@ import "../../Styles/body.css";
 export default function Homepage(){
   return(
     <div className="gameDiv">
-      <iframe className="game" frameborder="0" src="https://itch.io/embed-upload/9441917?color=333333" allowfullscreen="false" width="1280" height="740">
-        <a href="https://blazingworks.itch.io/networkedplay"></a>
-      </iframe>
+      <iframe className="game" frameborder="0" src="https://itch.io/embed-upload/9441917?color=333333" allowfullscreen="false" width="1280" height="740"><a href="https://blazingworks.itch.io/networkedplay">Play Networkedplay on itch.io</a></iframe>
     </div>
   );
 }
