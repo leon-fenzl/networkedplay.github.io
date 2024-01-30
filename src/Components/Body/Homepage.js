ec87fcf56978d646026fa1e892cad86c69339cf8
Blaze <32179683+leon-fenzl@users.noreply.github.com>
2024-01-30 20:12:04 -0300

    Update site

--- src/Components/Body/Homepage.js
+++ src/Components/Body/Homepage.js
@@ -3,10 +3,14 @@ import "../../Styles/body.css";
 export default function Homepage(){
   return(
     <div className="gameDiv">
-      <iframe frameborder="0" src="https://itch.io/embed-upload/9613862?color=333333" allowfullscreen="" width="1280" height="740">
+      <iframe frameborder="0" src="https://itch.io/embed-upload/9613862?color=333333" allowfullscreen="" width="1280" height="720">
         <a href="https://blazingworks.itch.io/networkedplay"></a>
       </iframe>
     </div>
   );
 }
-/*<a href="https://blazingworks.itch.io/networkedplay"></a>*/
+/*
+  <iframe frameborder="0" src="https://itch.io/embed-upload/9613862?color=333333" allowfullscreen="" width="1280" height="720">
+    <a href="https://blazingworks.itch.io/networkedplay"></a>
+  </iframe>
+*/
