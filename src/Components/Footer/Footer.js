d252b16caf9407a2658440b30c6cdbcad184b63f
Blaze <32179683+leon-fenzl@users.noreply.github.com>
2024-01-31 15:34:24 -0300

    saving code

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,8 +3,10 @@ import React from "react";
 export default function Footer(){
   return(
     <footer className="footer">
-      <h4>All rights reserved to </h4>
-      <h4 className="paulaItalic">Paula Nishijima</h4>
+      <pre className="footer-text">
+        Supported by ARTeCHÓ, co-funded by the European Union, initiative created by SERN – Startup Europe Regions Network (Belgium),Baltan Laboratories (Netherlands),
+        FZC-Etopia Center for Art & Technology (Spain), Frankfurt School Blockchain Center (Germany) and MEET Digital Cultural Center (Italy).
+      </pre>
     </footer>
   );
 }
