9d78c65dc292b67f8bba3d08aa93cb6de85497c2
Blaze <32179683+leon-fenzl@users.noreply.github.com>
2024-02-01 16:14:13 -0300

    button update error page

--- src/Components/Body/Homepage.js
+++ src/Components/Body/Homepage.js
@@ -1,11 +1,13 @@
 import React from "react";
+import {Link} from 'react-router-dom';
 import "../../Styles/body.css";
 export default function Homepage(){
   return(
     <div className="homepage">
-      <a href="/game">
-        <button className="btt-play"><h1>Play!</h1></button>
-      </a>
+      <Link to="/game" className="links"><button className="btt-play"><h1>Play!</h1></button></Link>
     </div>
   );
 }
+/*<a href="/game">
+<button className="btt-play"><h1>Play!</h1></button>
+</a>*/
