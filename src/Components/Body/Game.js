1eba932fa35312a42b89d0872ba54472184677a7
Blaze <32179683+leon-fenzl@users.noreply.github.com>
2024-02-04 10:40:02 -0300

    Update relink embedding

--- src/Components/Body/Game.js
+++ src/Components/Body/Game.js
@@ -3,8 +3,8 @@ import "../Styles/body.css";
 export default function Game(){
   return(
     <div className="gameDiv">
-      <iframe frameborder="0" src="https://itch.io/embed-upload/9613862?color=333333" allowfullscreen="" width="1280" height="720">
-        <a href="https://blazingworks.itch.io/networkedplay"></a>
+      <iframe frameborder="0" src="https://itch.io/embed-upload/9655483?color=333333" allowfullscreen="" width="1280" height="720">
+        <a href="https://blazingworks.itch.io/networkedplay">Play Networkedplay on itch.io</a>
       </iframe>
     </div>
   );
