7b432263d164d53456fd5139249696dca0ac7178
Blaze <32179683+leon-fenzl@users.noreply.github.com>
2024-02-17 17:56:44 -0300

    rebuild game link

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,19 +2,17 @@ import React from "react";
 import "./Styles/footer.css";
 export default function Footer(){
   return(
-    <div className="footercustom">
-      <div className="footer-flexbox">
-        <p className="footer-text">
-          Supported by ARTeCHÓ, co-funded by the European Union, 
-          initiative created by SERN – Startup Europe Regions Network (Belgium), 
-          Baltan Laboratories (Netherlands), 
-          FZC-Etopia Center for Art & Technology (Spain), 
-        </p>
-        <p className="footer-text">
-          Frankfurt School Blockchain Center (Germany) 
-          and MEET Digital Cultural Center (Italy).
-        </p>
-      </div>
+    <div className="footer-flexbox">
+      <p className="footer-text">
+        Supported by ARTeCHÓ, co-funded by the European Union, 
+        initiative created by SERN – Startup Europe Regions Network (Belgium), 
+        Baltan Laboratories (Netherlands), 
+        FZC-Etopia Center for Art & Technology (Spain), 
+      </p>
+      <p className="footer-text">
+        Frankfurt School Blockchain Center (Germany) 
+        and MEET Digital Cultural Center (Italy).
+      </p>
     </div>
   );
 }
--- src/Pages/Game.js
+++ src/Pages/Game.js
@@ -4,17 +4,15 @@ import Footer from "../Components/Footer.js";
 import "../Components/Styles/gameBody.css";
 export default function Game(){
   return(
-    <div className="container">
+    <div className="gridCon">
       <nav><Navbar/></nav>
-      <div className="empty1">empty1</div>
-        <div className="empty2"></div>
+        <div className="empty1"></div>
         <div className="gameDiv game" >
-        <iframe frameborder="0" src="https://itch.io/embed-upload/9725097?color=333333" allowfullscreen="" width="1280" height="720">
+        <iframe frameborder="0" src="https://itch.io/embed-upload/9743362?color=333333" allowfullscreen="" width="1280" height="740">
           <a href="https://blazingworks.itch.io/networkedplay"></a>
           </iframe>
         </div>
-        <div className="empty3"></div>
-        <div className="empty4"></div>
+        <div className="empty2"></div>
       <footer><Footer/></footer>
     </div>
   );
