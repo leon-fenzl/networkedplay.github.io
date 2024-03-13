da857cf8cd30c0a5490803f8b4af959b95517887
Blaze <32179683+leon-fenzl@users.noreply.github.com>
2024-03-13 11:52:59 -0300

    game rendering issue fix

--- src/Pages/Game.js
+++ src/Pages/Game.js
@@ -9,9 +9,9 @@ export default function Game(){
       <nav><Navbar/></nav>
         <div className="empty1"></div>
         <div className="gameDiv game" >
-        <iframe frameborder="0" src="https://itch.io/embed-upload/9879832?color=333333" allowfullscreen="" width="1280" height="720">
-          <a href="https://blazingworks.itch.io/networkedplay"></a>
-        </iframe>
+          <iframe frameborder="0" src="https://itch.io/embed-upload/9914352?color=333333" allowfullscreen="" width="1280" height="720">
+            <a href="https://blazingworks.itch.io/networkedplay"></a>
+          </iframe>
         </div>
         <div className="empty2"></div>
       <footer><Footer/></footer>
