7d8d94165669b7cff93ff554157424b488b1969e
Blaze <32179683+leon-fenzl@users.noreply.github.com>
2024-04-17 20:25:47 -0300

    Zoom In Ou Pan Rebuild

--- src/Pages/Research.js
+++ src/Pages/Research.js
@@ -4,7 +4,9 @@ import {Link} from "react-router-dom";
 import Navbar from "../Components/Navbar.js";
 import Footer from "../Components/Footer.js";
 import fsBtt from "../Components/images/fsButton.png";
-import imgResearch from "../Components/images/research page/Networked_play_research_mapping2_no_credits.png";
+import imgResearch_sm from "../Components/images/research page/Networked_play_research_mapping_EN.png";
+import ReactImageMagnify from 'react-image-magnify';
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
 export default function Research(){
   const handle = useFullScreenHandle();
@@ -14,10 +16,12 @@ export default function Research(){
       <div className="empty1"></div>
       <div className="gridCenter">
         <div className="research-contents">
-          <div>
-            <FullScreen className="fsDiv" handle={handle}><img src={imgResearch} className="imgSrc" id="imgSrc"/></FullScreen >
-            <div className="fsbttHolder"><img src={fsBtt}className="fsbtt" onClick={handle.enter}/></div>
-          </div>
+        <TransformWrapper>
+          <TransformComponent>
+            <img src={imgResearch_sm} className="resImg" alt="test" />
+          </TransformComponent>
+        </TransformWrapper>
+        <div><p>Mouse/Touch for Pan - Mouse Wheel/Pinch for Zoom In or Out</p></div>
         </div>
       </div>
       <div className="empty2"></div>
@@ -25,3 +29,21 @@ export default function Research(){
     </div>
   );
 }
+
+/*
+<FullScreen className="fsDiv" handle={handle}><img src={imgResearch} className="resImg" id="imgSrc"/></FullScreen >
+  <div className="fsbttHolder"><img src={fsBtt}className="fsbtt" onClick={handle.enter}/></div>
+
+  <ReactImageMagnify {...{
+    smallImage: {
+      alt: 'map-research',
+      isFluidWidth: true,
+      src: imgResearch_sm
+    },
+    largeImage: {
+      src: imgResearch_lg,
+      width: 6000,
+      height:2000
+    }
+  }}/>
+*/
